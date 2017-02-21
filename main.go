3df6668855ac830a643ba69808a57c07799eb240
Alain Gilbert <alain.gilbert.15@gmail.com>
2017-02-21 01:16:24 -0800

    ensure index

--- main.go
+++ main.go
@@ -47,9 +47,26 @@ type Poll struct {
 }
 func mainHandler(c echo.Context) error {
 	return c.String(200, "Hello World")
+func ensureIndex(s *mgo.Session) {
+	s2 := s.Copy()
+	defer s2.Close()
+	c := session.DB("poll").C("users")
+	index := mgo.Index{
+		Key:        []string{"twitterid"},
+		Unique:     true,
+		DropDups:   true,
+		Background: true,
+		Sparse:     true,
+	}
+	err := c.EnsureIndex(index)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func start(c *cli.Context) error {
+	ensureIndex(session)
+
 	t := &Template{}
 	port := c.Int("port")
 	e := echo.New()
