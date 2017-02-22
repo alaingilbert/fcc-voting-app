7706df8b72eea8cd5788f1a149544d0917e9a533
Alain Gilbert <alain.gilbert.15@gmail.com>
2017-02-21 20:54:16 -0800

    fix heroku

--- main.go
+++ main.go
@@ -448,7 +448,7 @@ func start(c *cli.Context) error {
 	}
 	defer session.Close()
 	session.SetMode(mgo.Monotonic, true)
-	ensureIndex()
+	//ensureIndex()
 
 	t := &Template{}
 	port := c.Int("port")
